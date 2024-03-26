71a1f6658ec957aaef9599b64f2e6fe47aa98984
35anurag <anuragregmi35@gmail.com>
2024-03-26 16:20:36 +0545

    Code updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,3 @@
-import "./Header.css";
 import Link from "next/link";
 import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 
@@ -12,13 +11,13 @@ import {
 
 const Header = () => {
   return (
-    <header className="navbar_header">
-      <div className="navbar_contents">
-        <div className="logo">
-          <h1>WellNest</h1>
+    <header className="border-b-2 mx-auto sticky z-10">
+      <div className="flex flex-row items-center justify-between py-2 md:py-3 lg:py-4 px-[1rem] md:px-[2rem] lg:px-[3rem]">
+        <div className="text-[25px] font-bold">
+          <Link href="/">WellNest</Link>
         </div>
 
-        <div className="navbar_links">
+        <div className=" flex-row items-center gap-[20px] list-none hidden lg:flex">
           <li>
             <Link href="/">Home</Link>
           </li>
@@ -33,7 +32,7 @@ const Header = () => {
           </li>
         </div>
 
-        <div className="nav_right">
+        <div className="flex flex-row items-center justify-center gap-[4px] md:gap-[4px] lg:gap-[8px]">
           <div>
             <SignedIn>
               <UserButton afterSignOutUrl="/" />
@@ -42,13 +41,28 @@ const Header = () => {
               <Link href="/sign-in">Login</Link>
             </SignedOut>
           </div>
-          <div className="mobile_nav">
+          <div className="block lg:hidden">
             <Sheet>
-              <SheetTrigger>Open</SheetTrigger>
+              <SheetTrigger className="mt-2">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+                  />
+                </svg>
+              </SheetTrigger>
               <SheetContent>
                 <SheetHeader>
                   <SheetTitle>
-                    <div className="mobile_links">
+                    <div className="flex flex-col items-center justify-center gap-[20px] list-none mt-[10rem]">
                       <li>
                         <Link href="/">Home</Link>
                       </li>
